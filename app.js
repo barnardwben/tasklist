fe77346dfe62256c945caafe01faff60585095c4
Benjamin Barnard <barnardwben@yahoo.com>
2020-06-20 10:59:13 +0200

    Updated Js

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ function storeTaskInLocalStorage(task) {
 
     tasks.push(task);
 
-    localStorage.setItem('tasks', JSON.stringify(tas))
+    localStorage.setItem('tasks', JSON.stringify(task))
   }
 }
 
