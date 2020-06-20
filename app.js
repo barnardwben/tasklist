a8595bc47f8b0d1e8193bb52ffbdfd27cc0f5af4
Benjamin Barnard <barnardwben@yahoo.com>
2020-06-20 11:09:02 +0200

    Updated Js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ function getTasks() {
   // Add class
   li.className = 'collection-item';
   // Create text node and append to li
-  li.appendChild(document.createTextNode(taskInput.value));
+  li.appendChild(document.createTextNode(task));
   // Create new link element
   const link = document.createElement('a');
   // Add class
